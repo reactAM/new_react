fb4a738a053fc22cd9be8ef0f4f61c5a434ad0ec
tubi55 <tubi55@nate.com>
2021-12-22 11:48:23 +0900

    좌우 버튼 추가

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './css/style.css';
 import { Logo, Menu } from './components/Header.js';
 import Panel from './components/Panel.js';
+import Btns from './components/Btns.js';
 import Footer from './components/Footer.js';
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
 				<Logo />
 				<Menu />				
 				<Panel />	
+				<Btns />
 				<Footer />
 			</figure>
 		</div>
---
+++ src/components/Btns.js
@@ -0,0 +1,11 @@
+
+function Btns(){
+  return (
+    <>
+      <span className="btnPrev">PREV</span>
+      <span className="btnNext">NEXT</span>
+    </>
+  )
+}
+
+export default Btns;
